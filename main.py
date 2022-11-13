593147127d335ef3cf7d38ac7a95ca80b4846309
PaulGo3 <116339350+PaulGo3@users.noreply.github.com>
2022-11-13 09:34:47 +0000

    csv

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 print("_______________________________________________________________________________")
-numpy.std()
+import csv
+print("Program to demonstrate csv.reader() function:")
+print("\n")
+c = open('csv-test.csv','r')
+print("The csv file is opened for reading:")
+print("\n")
+
+o = csv.reader(c)
+print("The contents of the above file is as follows:")
+for r in o:
+    print (r)
+c.close()
