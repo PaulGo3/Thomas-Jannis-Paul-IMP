38e0e3ec0a7ddead718223266c11a21608577921
PaulGo3 <116339350+PaulGo3@users.noreply.github.com>
2022-11-13 18:29:31 +0000

    standartabweichung

--- main.py
+++ main.py
@@ -1,28 +1,18 @@
+y = (100,98,101,93,94,91,90,90,97,93,93,95,93,91,89,89,88,89,90,91,94,93,94,96,92,93,93,92,91,90,90,89,91,95,97,93,92,95,98,93,91,96,98,95,93,96,93,93,95,92,90,88,87,91,89,90,88,87,89,86,86,89,92,90,92,93,91,89,87,86,88,87,90,93,92,90,89,91,93,90,92,94)
+
 print("_______________________________________________________________________________")
-import csv
-print("Program to demonstrate csv.reader() function:")
-print("\n")
-c = open('csv-test.csv','r')
-print("The csv file is opened for reading:")
-print("\n")
-
-o = csv.reader(c)
-print("The contents of the above file is as follows:")
-for r in o:
-    print (r)
-c.close()
 
 import csv
 
-with open('csv-test.csv', newline='') as f:
+with open('csv-test.csv') as f:
     reader = csv.reader(f)
     data = list(reader)
-
 print(data)
 
 import numpy as np
 
 arr = (10,20,30)
 print("1-D array :", arr)
-print("Standard Deviation of arr is ", np.std(arr))
+print("Standartabweichung ist: ", np.std(y))
+
 
